2e17c0dd48380ef99281ca2351f204f450750ac2
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-03-15 16:27:16 +0800

    Loops Basics

---
+++ LoopsBasics/LoopsBasics/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter go or stay");
+
+string userChoice = Console.ReadLine();
+
+while(userChoice == "go")
+{
+    Console.WriteLine("Go for a mile");
+    Console.WriteLine("Wanna keep going? Enter go.");
+    userChoice = Console.ReadLine();
+}
+Console.WriteLine("Finally you are staying!");
+
+Console.ReadKey();
