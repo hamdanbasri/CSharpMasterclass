05ce3fef81672626d4fbb3f39085365890e2760f
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-03-18 16:44:53 +0800

    Quiz App Setup

---
+++ MyQuizApp/MyQuizApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyQuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question("What is the capital of Malaysia?", // QUESTION TEXT
+                    new string[] {"Kuala Lumpur", "Johor", "Pahang", "Putrajaya"}, // ANSWER ARRAY
+                    0 // CORRECT ANSWER INDEX
+                )
+            };
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.DisplayQuestion(questions[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyQuizApp/MyQuizApp/Quiz.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizApp
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+        public void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(question.QuestionText);
+            Console.ResetColor();
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+
+            if( GetUserChoice() == question.CorrectAnswerIndex)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer (number): ");
+            string input = Console.ReadLine();
+            int choice = 0;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4");
+                input = Console.ReadLine();
+            }
+
+            return choice - 1; // adjust to 0-indexed array
+        }
+    }
+}
