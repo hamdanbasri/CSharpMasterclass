6028a4c3044529abb913428c984e602fe09b727a
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-03-30 20:56:28 +0800

    Section 7 Error Handling Completed

---
+++ AdvancedExercise_HandlingMultipleExceptions/AdvancedExercise_HandlingMultipleExceptions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace AdvancedExercise_HandlingMultipleExceptions
+{
+    //Create a simple C# program that demonstrates handling multiple types of exceptions. The program should:
+    //Declare a method HandleMultipleExceptions which:
+    //Takes a string and an int as parameters, in that order.
+    //Contains a local int[] variable with predefined elements, for example, int[] numbers = { 1, 2, 3 };.
+    //Contains a try block where you:
+    //Attempt to parse an integer from the string parameter that could throw a FormatException.
+    //Access an element in the array by the integer parameter as an index that could throw an IndexOutOfRangeException.
+    //Catch and handle both FormatException and IndexOutOfRangeException.
+    //Print specific messages for each exception type.
+    //Alert!
+    //The result of execution should be:
+    //If a FormatException occurs: "Invalid format."
+    //If an IndexOutOfRangeException occurs: "Index out of range."
+    //If no exception occurs: the accessed array element.
+    //Example output for no exception: "2"
+    //Example output for FormatException: "Invalid format."
+    //Example output for IndexOutOfRangeException: "Index out of range."
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Write("Main method is running");
+            HandleMultipleExceptions("bla", 5);
+            Console.ReadKey();
+        }
+
+        public static void HandleMultipleExceptions(string myString, int myInt)
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                int parsedValue = int.Parse(myString);
+                Console.WriteLine($"{numbers[myInt]}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Index out of range."+ ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+        }
+    }
+}
---
+++ TryCatchException/TryCatchException/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+namespace TryCatchException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Debug.Write("Main method is running");
+
+            try
+            {
+                Console.WriteLine("Please enter a number:");
+                int num1 = int.Parse(Console.ReadLine());
+                //int num1 = 0;
+                int num2 = 2;
+                result = num2 / num1;
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine($"Dont divide by zero {ex.Message}");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine($"Please enter a number not a letter: {ex.Message}");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Number Too High");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Debug.WriteLine(ex.ToString());
+            }finally
+            {
+                Console.WriteLine("This always executes");
+            }
+
+            Console.WriteLine($"Result: {result}");
+
+            Console.WriteLine("Enter Your Age: ");
+            GetUserAge(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+
+        static int GetUserAge(string input)
+        {
+            int age;
+            if(!int.TryParse(input, out age))
+            {
+                throw new Exception("You didn't enter a valid age.");
+            }
+            if(age < 0 || age > 120)
+            {
+                throw new Exception("Your age must be between 0 and 120");
+            }
+            return age;
+        }
+    }
+}
