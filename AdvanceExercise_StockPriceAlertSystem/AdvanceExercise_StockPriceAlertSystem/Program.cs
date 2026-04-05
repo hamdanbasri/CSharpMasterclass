1a931c0b7eec99162ede281e64ba20c136547ff0
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-04-05 22:03:13 +0800

    Section 10 Completed

---
+++ AdvanceExercise_StockPriceAlertSystem/AdvanceExercise_StockPriceAlertSystem/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace AdvanceExercise_StockPriceAlertSystem
+{
+    // Define the delegate that will be used for the event
+    public delegate void StockPriceChangedHandler(string message);
+
+    // Define the Stock class which includes the event system
+    public class Stock
+    {
+        // Declare the event using the delegate
+        // Private field to store the stock price
+        // Private field to store the threshold
+        //TODO
+        public event StockPriceChangedHandler OnStockPriceChanged;
+        private decimal _price;
+        private decimal _threshold;
+
+        // Property to get and set the stock price        
+        public decimal Price
+        {
+            // Set the new price
+            // Raise the event if the price drops below the threshold
+            //TODO
+            get {  return _price; }
+            set 
+            { 
+                _price = value; 
+                if(_price < _threshold)
+                {
+                    RaiseStockPriceChangedEvent("Stock price is below threshold!");
+                }
+            }
+
+        }
+
+        // Property to get and set the alert threshold
+        public decimal Threshold
+        {
+            //TODO
+            get { return _threshold; }
+            set
+            {
+                _threshold = value;
+            }
+        }
+        protected virtual void RaiseStockPriceChangedEvent(string message)
+        {
+            // Invoke the event
+            //TODO
+            OnStockPriceChanged?.Invoke(message);
+        }
+    }
+
+    // Define the subscriber class which reacts to the event
+    public class StockAlert
+    {
+        // Method that handles the event and prints a message to the console
+        //TODO
+        public void OnStockPriceChanged(string message)
+        {
+            Console.WriteLine($"Stock Alert: {message}");
+        }
+    }
+
+    // Program class to simulate the stock price changes and test the event system
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Stock and StockAlert
+            //TODO
+            Stock stock = new Stock();
+            StockAlert stockAlert = new StockAlert();
+
+            // Subscribe to the stock price changed event
+            //TODO
+            stock.OnStockPriceChanged += stockAlert.OnStockPriceChanged;
+
+            // Set the alert threshold
+            //TODO
+            stock.Threshold = 120;
+
+            // Simulate stock price changes
+            //TODO
+            stock.Price = 150;
+            stock.Price = 130;
+            stock.Price = 110;
+
+            // Wait for user input to close the console
+            //TODO
+            Console.ReadKey();
+        }
+    }
+}
--- TemperatureMonitorApp/TemperatureMonitorApp/Program.cs
+++ TemperatureMonitorApp/TemperatureMonitorApp/Program.cs
@@ -1,35 +1,69 @@
 ﻿namespace TemperatureMonitorApp
 {
-    public delegate void TemperatureChangeHandler(string message);
+    // Using the generic delegate EventHandler<TEventArgs>
 
+    //public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureChangedEventArgs : EventArgs
+    {
+        // Property holding the temperature
+        public int Temperature { get; }
+
+        // Constructor
+        public TemperatureChangedEventArgs(int temperature)
+        {
+            Temperature = temperature;
+        }
+    }
     public class TemperatureMonitor
     {
-        public event TemperatureChangeHandler OnTemperatureChange;
+        public event EventHandler<TemperatureChangedEventArgs> TemperatureChanged;
+        //public event TemperatureChangeHandler OnTemperatureChange;
         private int _temparature;
         public int Temperature
         {
             get { return _temparature; }
             set
             {
-                _temparature = value;
-                if (_temparature > 30)
+                if (_temparature != value)
                 {
-                    RaiseTemperatureChangedEvent("Temperature is above threshold");
+                    _temparature = value;
+                    OnTemperatureChanged(new TemperatureChangedEventArgs(value));
                 }
+                //_temparature = value;
+                //if (_temparature > 30)
+                //{
+                //    RaiseTemperatureChangedEvent("Temperature is above threshold");
+                //}
             }
         }
 
-        protected virtual void RaiseTemperatureChangedEvent(string message)
+        protected virtual void OnTemperatureChanged(TemperatureChangedEventArgs e)
         {
-            OnTemperatureChange?.Invoke(message);
+            TemperatureChanged?.Invoke(this, e);
         }
 
+        //protected virtual void RaiseTemperatureChangedEvent(TemperatureChangedEventArgs e)
+        //{
+        //    OnTemperatureChange?.Invoke(e);
+        //}
+
+        //protected virtual void RaiseTemperatureChangedEvent(string message)
+        //{
+        //    OnTemperatureChange?.Invoke(message);
+        //}
+
+        // Subscriber
         public class TemperatureAlert
         {
-            public void OnTemperatureChanged(string message)
+            public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e)
             {
-                Console.WriteLine($"Alert: {message}");
+                Console.WriteLine($"Alert: temperature is {e.Temperature}, sender is {sender}");
             }
+            //public void OnTemperatureChanged(string message)
+            //{
+            //    Console.WriteLine($"Alert: {message}");
+            //}
         }
         internal class Program
         {
@@ -37,10 +71,12 @@
             {
                 TemperatureMonitor monitor = new TemperatureMonitor();
                 TemperatureAlert alert = new TemperatureAlert();
-                monitor.OnTemperatureChange += alert.OnTemperatureChanged;
+
+                monitor.TemperatureChanged += alert.OnTemperatureChanged;
+                //monitor.OnTemperatureChange += alert.OnTemperatureChanged;
 
                 monitor.Temperature = 20;
-                Console.WriteLine("Please enter the temparature");
+                Console.WriteLine("Please enter the temperature");
                 monitor.Temperature = int.Parse(Console.ReadLine());
 
                 Console.ReadKey();
