b2f58301612d7f4ef99c1e85b72d749ed3e14018
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-04-06 11:09:00 +0800

    Section 12: Regular Expression Completed

---
+++ AdvanceExercise_ExtractEmailRegex/AdvanceExercise_ExtractEmailRegex/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdvanceExercise_ExtractEmailRegex
+{
+    public class Exercise
+    {
+
+        public void ExtractPatterns(string input)
+        {
+            // TODO: Implement the method to extract and print email addresses using regex
+            //string email_1 = "support@example.com";
+            //string email_2 = "sales@example.org";
+            string pattern = @"(\w+)\@\w+\.\w+";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            foreach (Match match in matchCollection) 
+            {
+                GroupCollection group = match.Groups;
+                Console.WriteLine($"{group[0].Value}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise exercise = new Exercise();
+            exercise.ExtractPatterns("support@example.com");            
+            exercise.ExtractPatterns("sales@example.org");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RegexE/RegexE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\d{5}";
+            //string pattern = @"there";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there, my number is 12314";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine($"{matchCollection.Count} hits found:\n {text}");
+
+            foreach (Match hit in matchCollection) 
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine($"{group[0].Value} found at {group[0].Index}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
