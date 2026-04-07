775319b4b89a42a332e1bed932087ee448ef8a0e
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-04-07 22:25:34 +0800

    Data Binding Example

---
+++ DataBindingWPF/DataBindingWPF/Data/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBindingWPF.Data
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
