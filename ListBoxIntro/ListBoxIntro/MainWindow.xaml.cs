625c359cbb78bc5a75134ef1155d6a1d5d8f1dd6
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-04-07 23:54:39 +0800

    ListBoxWPF Intro

---
+++ ListBoxIntro/ListBoxIntro/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBoxIntro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> People = new List<Person>
+        {
+            new Person{Name = "Hamdan", Age = 34},
+            new Person{Name = "Hada", Age = 31},
+            new Person{Name = "Idrees", Age = 6},
+            new Person{Name = "Ireena", Age = 2}
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBoxPeople.ItemsSource = People;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListBoxPeople.SelectedItems;
+
+            foreach (var item in selectedItems) 
+            {
+                var person = (Person)item;
+                MessageBox.Show(person.Name);
+            }
+        }
+    }
+}
