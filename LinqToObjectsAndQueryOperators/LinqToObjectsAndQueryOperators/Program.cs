29d647210d60e12f722136dbf9b11205f4091d90
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-04-15 15:17:31 +0800

    Linq To Object

--- LinqToObjectsAndQueryOperators/LinqToObjectsAndQueryOperators/Program.cs
+++ LinqToObjectsAndQueryOperators/LinqToObjectsAndQueryOperators/Program.cs
@@ -13,7 +13,46 @@ namespace LinqToObjectsAndQueryOperators
             UniversityManager um = new UniversityManager();
 
             um.MaleStudent();
+            Console.WriteLine("\n");
             um.FemaleStudent();
+            Console.WriteLine("\n");
+            um.SortStudentByAge();
+            Console.WriteLine("\n");
+            um.AllStudentFromBeijingTech();
+            Console.WriteLine("\n");
+            um.AllStudentFromYale();
+            Console.WriteLine("\n");
+            um.StudentAndUniversityNameCollection();
+            Console.WriteLine("\n");
+
+            int[] someInts = { 30, 12, 4, 3, 12 };
+            IEnumerable<int> sortedInts = from i in someInts orderby i select i;
+            IEnumerable<int> reversedInts = sortedInts.Reverse();
+
+            foreach (int i in reversedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            IEnumerable<int> reversedSortedInts = from i in someInts orderby i descending select i;
+
+            foreach (int i in reversedSortedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Enter the University ID: ");
+            string input = Console.ReadLine();
+            try
+            {
+                int inputAsInt = Convert.ToInt32(input);
+
+                um.AllStudentsFromThatUni(inputAsInt);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong value");
+            }
 
             Console.ReadKey();
         }
--- LinqToObjectsAndQueryOperators/LinqToObjectsAndQueryOperators/UniversityManager.cs
+++ LinqToObjectsAndQueryOperators/LinqToObjectsAndQueryOperators/UniversityManager.cs
@@ -32,7 +32,7 @@ namespace LinqToObjectsAndQueryOperators
         public void MaleStudent()
         {
             IEnumerable<Student> malestudents = from student in students where student.Gender == "male" select student;
-            Console.WriteLine("Male - Students");
+            Console.WriteLine("[----- MALE - STUDENTS -----]");
 
             foreach (Student student in malestudents) 
             {
@@ -43,11 +43,80 @@ namespace LinqToObjectsAndQueryOperators
         public void FemaleStudent()
         {
             IEnumerable<Student> femalestudents = from student in students where student.Gender == "female" select student;
-            Console.WriteLine("Female - Students");
+            Console.WriteLine("[----- FEMALE - STUDENTS -----]");
 
             foreach (Student student in femalestudents)
             {
                 student.Print();
+            }
+        }
+
+        public void SortStudentByAge()
+        {
+            var sortedStudents = from student in students orderby student.Age select student;
+            Console.WriteLine("[----- STUDENT SORTED BY AGE -----]");
+
+            foreach(Student student in sortedStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentFromBeijingTech()
+        {
+            IEnumerable<Student> beijingStudents = from student in students 
+                                               join university in universities on student.UniversityId equals university.Id 
+                                               where university.Name == "Beijing Tech" 
+                                               select student;
+
+            Console.WriteLine("[----- STUDENT FROM BEIJING TECH -----]");
+            foreach(Student student in beijingStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentFromYale()
+        {
+            IEnumerable<Student> yaleStudents = from student in students
+                                                   join university in universities on student.UniversityId equals university.Id
+                                                   where university.Name == "Yale"
+                                                   select student;
+
+            Console.WriteLine("[----- STUDENT FROM YALE -----]");
+            foreach (Student student in yaleStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentsFromThatUni(int Id)
+        {
+            IEnumerable<Student> myStudents = from student in students
+                                              join university in universities on student.UniversityId equals university.Id
+                                              where university.Id == Id
+                                              select student;
+
+            Console.WriteLine("Students from that uni {0}", Id);
+            foreach (Student student in myStudents)
+            {
+                student.Print();
+            }
+
+        }
+
+        public void StudentAndUniversityNameCollection()
+        {
+            var newCollection = from student in students
+                                join university in universities on student.UniversityId equals university.Id
+                                orderby student.Name
+                                select new { StudentName = student.Name, UniverstityName = university.Name };
+
+            Console.WriteLine("New Collection: ");
+
+            foreach(var col in newCollection)
+            {
+                Console.WriteLine($"Student {col.StudentName} from University {col.UniverstityName}");
             }
         }
     }
