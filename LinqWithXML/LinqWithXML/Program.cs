67f5017f8d454d48b4bcef41d4f23aa45b7dd456
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-04-15 15:33:43 +0800

    LinqWithXML

---
+++ LinqWithXML/LinqWithXML/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqWithXML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //We simply apply our Student-Structure to XML. 
+            string studentsXML =
+                        @"<Students>
+                            <Student>
+                                <Name>Toni</Name>
+                                <Age>21</Age>
+                                <University>Yale</University>
+                                <Semester>6</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Carla</Name>
+                                <Age>17</Age>
+                                <University>Yale</University>
+                                <Semester>1</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Leyla</Name>
+                                <Age>19</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>3</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Frank</Name>
+                                <Age>25</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>10</Semester>
+                            </Student>
+                        </Students>";
+
+            XDocument studentXdoc = new XDocument();
+            studentXdoc = XDocument.Parse(studentsXML);
+
+            var students = from student in studentXdoc.Descendants("Student")
+                           select new
+                           {
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value,
+                               Semester = student.Element("Semester").Value
+                           };
+
+            Console.WriteLine("Showing All Students");
+            
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Student Name: {student.Name}, Aged: {student.Age} from {student.University} is in the {student.Semester} semester");
+            }
+
+            var sortedStudents = from student in students
+                                 orderby student.Age
+                                 select student;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Students Sorted by Age");
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine("Student {0} with age {1} from University {2}", student.Name, student.Age, student.University);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
